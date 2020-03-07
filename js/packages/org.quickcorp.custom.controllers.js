dee87b105a778abde8724a0c258f042cf1fd63f7
Quickcorp <info@quickcorp.cl>
2020-03-07 17:23:01 -0300

    segment

--- js/packages/org.quickcorp.custom.controllers.js
+++ js/packages/org.quickcorp.custom.controllers.js
@@ -102,6 +102,14 @@ Package('org.quickcorp.custom.controllers',[
 	  },
 		done: function (){
 //			this.loadInstallerButton();
+
+			// segment stats
+			!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
+		   analytics.load("97lbKH8VJd9AAmTExODzIXl2v5nSuxhB");
+		   analytics.page();
+		 }}();
+			// end segment stats
+
 			this.component.body.subelements('button[name=download]')[0].addEventListener('click',function (event){
 				location.href="https://www.leychile.cl/Consulta/Exportar?radioExportar=Normas&exportar_formato=pdf&nombrearchivo=DTO-100_22-SEP-2005&exportar_con_notas_bcn=True&exportar_con_notas_originales=True&exportar_con_notas_al_pie=True&hddResultadoExportar=242302.2019-12-24.0.0%23";
 			});
